cf74265faae63c78a97387deb67ca5321d958af6
Arjun <a12jun@gmail.com>
2016-11-11 15:24:30 +0000

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-resume serve --theme paper > /dev/null &
+echo "About to serve resume..."
+resume serve --theme elegant --port 4000 > /dev/null &
 PID=$!
 
-sleep 4
+sleep 2
+
+echo "Saving as html"
+wget localhost:4000 -O out/resume.html
+
+echo "Done"
