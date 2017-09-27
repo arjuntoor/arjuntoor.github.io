1d4d556c34cac99d14c58a0b7418292d6d580083
Arjun S Toor <arjun.toor@bt.com>
2017-09-27 23:38:03 +0100

    Added script to run the conversion

---
+++ build-resume.js
@@ -0,0 +1,35 @@
+
+var objgen = require('objgen/objgen').ObjGen
+var fs = require('fs');
+
+fs.readFile('resume.txt', 'utf8', function(err, raw_file) {  
+    if (err) console.log(err);
+    
+    var json = convertToJson(raw_file);
+    saveFile(json, {name:'resume.json', overwrite:true});
+});
+
+
+/**
+ * Converts input to json using ObjGen. 
+ * Input must be in ObjGen format
+ * @param {*} input 
+ */
+function convertToJson(input) {
+    console.log("Converting the resume to json");
+    return objgen.xJson(input, {numSpaces: 2});
+}
+
+
+/**
+ * Saves the given content to a file
+ * @param {*} json 
+ * @param {*} options 
+ */
+function saveFile(content, options) {
+    var dest = options.name;
+    fs.writeFile(dest, content, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('File saved to ' + dest);
+    });
+}
