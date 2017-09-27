cbfd8946b8df3e4800777e044c068a3dc78b85bb
Arjun S Toor <arjun.toor@bt.com>
2017-09-27 23:45:14 +0100

    Added some comments to the vagrant script

--- vagrant-scripts/copy-project-to-home.sh
+++ vagrant-scripts/copy-project-to-home.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# This copies the project to the home directory in the vagrant box so that we are able to 'npm install'.  
+# You can't do that in a vagrant mounted folder.
+
+# Currently this should be run from inside vagrant
 
 cp -rf /vagrant/* /home/vagrant/my-resume/
