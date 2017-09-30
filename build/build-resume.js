5c40bd964ac4d05ba24d587db0afd5b526346f1e
Arjun <a12jun@gmail.com>
2017-09-30 10:22:48 +0100

    No longer need the python script to convert string arrays, it's done as part of the npm convert script

--- build/build-resume.js
+++ build/build-resume.js
@@ -1,11 +1,18 @@
-
-var objgen = require('objgen/objgen').ObjGen
-var fs = require('fs');
+// Imports
+const objgen = require('objgen/objgen').ObjGen
+const fs = require('fs');
 const readline = require('readline');
+
+// Other constants
+const resumeRaw = 'src/resume.txt';
+const resumeOut = 'build/resume.json';
+const resumeEncoding = 'utf8';
+const stringArrayStartTag = '#startStringArray';
+const stringArrayEndTag = '#endStringArray';
 
 
 function main() {
-    fs.readFile('src/resume.txt', 'utf8', buildResume);
+    fs.readFile(resumeRaw, resumeEncoding, buildResume);
 }
 
 /**
@@ -15,25 +22,51 @@ function main() {
  */
 function buildResume(error, rawFileContents) {  
     if (error) throw error;
-    var intermediate = convertStringArray(rawFileContents);
-    //var json = convertToJson(intermediate);
-    //saveFile(json, {name:'dist/resume.json'});
+    var intermediate = convertStringArrays(rawFileContents);
+    //console.log(intermediate);
+    var json = convertToJson(intermediate);
+    saveFile(json, {name:resumeOut});
 }
 
 
-function convertStringArray(contents) {
-    const rl = readline.createInterface({
-        input: fs.createReadStream('src/resume.txt')
-    });
+function convertStringArrays(contents) {  
+    var lines = contents.split('\n');
+    var resultLines = [];
+    var line;
     
-    rl.on('line', (line) => {
-        if (line.indexOf("#startStringArray") > -1) {
-            console.log(line);
+    for (var i=0; i<lines.length; i++) {
+        line = lines[i];
+        
+        if (line.indexOf(stringArrayStartTag) > -1) {
+            var arrayName = line.split(' ')[1];
+            
+            i++; //advance to first line after start tag
+            
+            var linesToMerge = [];
+            
+            while (lines[i].indexOf(stringArrayEndTag) == -1) {
+                linesToMerge.push(normalise(lines[i]));
+                i++;
+            }
+            i++; // move past end array tag
+            line = `  highlights [] s = ${linesToMerge.join(',')}`;
         }
-    });
-
+        
+        resultLines.push(line);
+    }
     
+    return resultLines.join('\n');
 }
+
+
+/**
+ * Converts the given line into the required format. 
+ * @param {*} line 
+ */
+function normalise(line) {
+    return line.replace(/,/g, '&#44;').replace('desc = ', '').trim();
+}
+
 
 /**
  * Converts input to json using ObjGen. 
@@ -53,9 +86,9 @@ function convertToJson(input) {
  */
 function saveFile(content, options) {
     var dest = options.name;
-    fs.writeFile(dest, content, 'utf8', (err) => {
+    fs.writeFile(dest, content, resumeEncoding, (err) => {
         if (err) throw err;
-        console.log('File saved to ' + dest);
+        console.log(`File saved to ${dest}`);
     });
 }
 
