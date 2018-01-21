5380838b285392bfefdf49bfef85cfc63412b120
Arjun S Toor <arjun.toor@bt.com>
2018-01-21 15:13:46 +0000

    Added deploy script for convenience

---
+++ build/deploy.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+var fsextra = require('fs-extra');
+var sleep = require('system-sleep');
+var prompt = require('prompt');
+
+var path = "dist/";
+var files = [];
+
+
+function loadFiles() {
+    fs.readdir(path, function(err, items) {
+        for (var i=0; i<items.length; i++) {
+            files[i] = {'id': i, 'file': items[i]};
+        }
+    });
+}
+
+function askWhichFile() {
+    var choice = -1;
+
+    console.log("Please pick which file you want to deploy");
+
+    for (var i=0; i<files.length; i++) {
+        console.log(`${files[i].id}.  ${files[i].file}`);
+    }
+    
+    prompt.start();
+    prompt.get('choice', function(err, result) {
+        if (err) {
+            console.log(err); return;
+        }
+        
+        try {
+            choice = parseInt(result.choice);
+            if (isNaN(choice) || choice >= files.length || choice < 0) {
+                console.log('Bad choice...');  return; 
+            }
+            deploy(choice);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+function deploy(choice) {
+    var file = files.find(function(e) {
+        return e.id == choice;
+    });
+    console.log(file);
+    fsextra.copySync(path + file['file'], "index.html");
+    console.log("Done");
+}
+
+function main() {
+    loadFiles();
+    sleep(2000);
+    askWhichFile();
+}
+
+
+main();
